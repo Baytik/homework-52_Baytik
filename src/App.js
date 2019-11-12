d18668922b08f4bea7e201a09997006f25f0d019
Baytik Tumanbaev <jumka.wows@gmail.com>
2019-11-12 15:24:52 +0600

    Writed first js code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './component/Person';
+import Circle from "./Circle/Circle";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+
+    };
+
+    generateNumbers = () => {
+      let array = [];
+      for (let i = 0; i < 5;) {
+          const number = Math.floor(Math.random() * 36) + 5;
+          if(array.includes(number)) {
+              continue;
+          } else {
+              number.push(array);
+          }
+      }
+    };
+
+  render() {
+        return (
+            <div className="App">
+
+            </div>
+
+        )
+  }
 }
 
 export default App;
---
+++ src/Circle/Circle.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+const Circle = props => {
+    return (
+        <div className="circle">
+            {props.number}
+        </div>
+    )
+}
+export default Circle;
