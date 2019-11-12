1ef6a081c5310433634be24991e88b9626c80c15
Baytik Tumanbaev <jumka.wows@gmail.com>
2019-11-12 17:48:33 +0600

    wrote fully scripts

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
 import React, {Component} from 'react';
 import './App.css';
-import Person from './component/Person';
-import Circle from "./Circle/Circle";
+import Circle from "./component/Circle/Circle";
+// import Person from './component/Person';
+// import Circle from "./Circle/Circle";
 
 class App extends Component {
     state = {
-
+        numbers: [],
     };
 
     generateNumbers = () => {
       let array = [];
       for (let i = 0; i < 5;) {
-          const number = Math.floor(Math.random() * 36) + 5;
+          const number = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+
+
           if(array.includes(number)) {
               continue;
           } else {
-              number.push(array);
+              array.push(number);
+              i++;
           }
       }
+        array.sort(function (a, b) {
+            return a - b
+        });
+      this.setState({
+          numbers: array,
+      })
     };
 
   render() {
         return (
             <div className="App">
-
+                {this.state.numbers.map(number => {
+                    return <Circle number={number}/>
+                })}
+                <button onClick={this.generateNumbers}>New numbers</button>
             </div>
-
         )
   }
 }
